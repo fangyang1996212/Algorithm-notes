aa5ad32e88f2d0775b0f48d02f414f14e7019ceb
fangyang1996212 <37264455+fangyang1996212@users.noreply.github.com>
2020-09-02 15:30:19 +0800

    Update 立方根.py

--- 立方根.py
+++ 立方根.py
@@ -5,7 +5,7 @@ def cube_root(cube, epsilon):
     positive_num  = abs(cube)
     low = 0
     high = positive_num
-    guess = (low + high) / 2.
+    guess = (low + high) / 2. # guess = low + (high - low) / 2.  防止(low + high)大于 int ，即大于65535，而溢出的写发。
  
     while abs(guess**3 - positive_num) > epsilon:
         if guess**3 > positive_num:
@@ -13,6 +13,6 @@ def cube_root(cube, epsilon):
         else:
             low = mid
  
-        guess = (low + high) / 2.
+        guess = (low + high) / 2. # guess = low + (high - low) / 2.  防止(low + high)大于 int ，即大于65535，而溢出的写发。
  
     return mid if cube>=0 else -mid
